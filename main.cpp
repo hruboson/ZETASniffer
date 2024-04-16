9b978b658e8e826bc48ffe7b8832900d103f4a2a
Ondřej Hruboš <hruboson@gmail.com>
2024-04-16 12:41:39 +0200

    MAC address

--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 int main(int argc, char *argv[])
 {
 	conf::Config conf{argc, argv};
-	conf.print();	
+	//conf.print();	
+
 	Sniffer sniffer{conf};
 	sniffer.sniff();
 	return 0;
--- sniffer.hpp
+++ sniffer.hpp
@@ -10,6 +10,7 @@
 #include <netinet/tcp.h>
 #include <netinet/udp.h>
 #include <netinet/ip_icmp.h>
+#include <netinet/ether.h>
 #include <ctime>
 #include <cmath>
 
@@ -24,8 +25,9 @@ private:
 
 	pcap_t* create_pcap_handle(const char* interface, const char* filter);
 	void get_link_layer_type_size(pcap_t* handle);
-	inline static void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packetptr){
+	inline static void packet_handler(u_char *user, const struct pcap_pkthdr *packethdr, const u_char *packet){
 		struct ip* iphdr;
+		struct ether_header* ehdr;
 		struct icmp* icmphdr;
 		struct tcphdr* tcphdr;
 		struct udphdr* udphdr;
@@ -34,6 +36,15 @@ private:
 		char dstip[256];
 		char srcmac[256];
 		char dstmac[256];
+
+		// get MAC from ethernet frame
+	    ehdr = (struct ether_header *) packet;
+		const struct ether_addr *eaddr_src = (const struct ether_addr*) &ehdr->ether_shost;
+		const struct ether_addr *eaddr_dst = (const struct ether_addr*) &ehdr->ether_dhost;
+		sprintf(srcmac, "%02x:%02x:%02x:%02x:%02x:%02x",eaddr_src->ether_addr_octet[0],eaddr_src->ether_addr_octet[1],eaddr_src->ether_addr_octet[2],eaddr_src->ether_addr_octet[3],eaddr_src->ether_addr_octet[4],eaddr_src->ether_addr_octet[5]);
+		sprintf(dstmac, "%02x:%02x:%02x:%02x:%02x:%02x",eaddr_dst->ether_addr_octet[0],eaddr_dst->ether_addr_octet[1],eaddr_dst->ether_addr_octet[2],eaddr_dst->ether_addr_octet[3],eaddr_dst->ether_addr_octet[4],eaddr_dst->ether_addr_octet[5]);
+
+		// timestamp
 		struct tm *timestamp_tm = localtime(&packethdr->ts.tv_sec); // convert to localtime for strftime
 		char timestamp[256];
 		strftime(timestamp, 256, "%Y-%m-%dT%H:%M:%S", timestamp_tm); // format time (without microseconds - will be added in std::cout)
@@ -41,29 +52,30 @@ private:
 	    int gmtoff_min = std::abs(timestamp_tm->tm_gmtoff % 3600) / 60;
 
 		// Skip the datalink layer header and get the IP header fields.
-		packetptr += Sniffer::link_layer_type_len;
-		iphdr = (struct ip*)packetptr;
+		packet += Sniffer::link_layer_type_len;
+		iphdr = (struct ip*)packet;
 		strcpy(srcip, inet_ntoa(iphdr->ip_src));
 		strcpy(dstip, inet_ntoa(iphdr->ip_dst));
 		
 	 
 		// Advance to the transport layer header then parse and display
 		// the fields based on the type of hearder: tcp, udp or icmp.
-		packetptr += 4*iphdr->ip_hl;
+		packet += 4*iphdr->ip_hl;
 		switch(iphdr->ip_p){
 			case IPPROTO_TCP:
-				tcphdr = (struct tcphdr*) packetptr;
+				tcphdr = (struct tcphdr*) packet;
 				break;
 			case IPPROTO_UDP:
-				udphdr = (struct udphdr*) packetptr;
+				udphdr = (struct udphdr*) packet;
 				break;
 		}
 		std::cout <<
-			"timestamp: " 
-						// this was painful :( timestamp_tm doen't have microseconds so you can't just do strftime
+			"timestamp: " // this was painful :( timestamp_tm doen't have microseconds so you can't just do strftime
 						<< timestamp << "." << std::setw(3) << std::setfill('0') << packethdr->ts.tv_usec / 1000 
-						<< "+" << std::setw(2) << std::setfill('0') << gmtoff_hour << ":" << std::setw(2) << std::setfill('0') << gmtoff_min << std::noshowpos << std::endl <<
-			//"src MAC: " << << std::endl;
+						<< "+" << std::setw(2) << std::setfill('0') << gmtoff_hour << ":" << std::setw(2) << std::setfill('0') << gmtoff_min << std::noshowpos << 
+						std::endl <<
+			"src MAC: " << srcmac << std::endl << 
+			"dst MAC: " << dstmac << std::endl <<
 			"frame length: " << ntohs(iphdr->ip_len) << std::endl <<
 			"src IP: " << srcip << std::endl <<
 			"dst IP: " << dstip << std::endl << std::endl;
