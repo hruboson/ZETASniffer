3c9fa466f983ed59f08ce195dfe3c31bd8c23b72
Ondřej Hruboš <hruboson@gmail.com>
2024-04-22 15:05:53 +0200

    Final update to filter builder

--- sniffer.cpp
+++ sniffer.cpp
@@ -20,29 +20,35 @@ Sniffer::Sniffer(conf::Config &config) : config{config}{
 	std::stringstream tmp_filter;
 
 	if(config.protocol() == conf::UDP){
-		tmp_filter << "udp ";
+		tmp_filter << "(udp ";
 		if(config.port() >= 0){
-			tmp_filter << "and port " << config.port() << ' ';
+			tmp_filter << "and port " << config.port();
 		}else{
 			if(config.port_s() >= 0){
-				tmp_filter << "and src port " << config.port_s() << ' ';
+				tmp_filter << "and src port " << config.port_s();
 			}
 			if(config.port_d() >= 0){
-				tmp_filter << "and dst port " << config.port_d() << ' ';
+				tmp_filter << "and dst port " << config.port_d();
 			}
 		}
-	}else if(config.protocol() == conf::TCP){
-		tmp_filter << "tcp ";
+		tmp_filter << ") ";
+	}
+	if(config.protocol() == conf::TCP){
+		if(tmp_filter.rdbuf()->in_avail() != 0){
+			tmp_filter << "or ";
+		}
+		tmp_filter << "(tcp ";
 		if(config.port() >= 0){
-			tmp_filter << "and port " << config.port() << ' ';
+			tmp_filter << "and port " << config.port();
 		}else{
 			if(config.port_s() >= 0){
-				tmp_filter << "and src port " << config.port_s() << ' ';
+				tmp_filter << "and src port " << config.port_s();
 			}
 			if(config.port_d() >= 0){
-				tmp_filter << "and dst port " << config.port_d() << ' ';
+				tmp_filter << "and dst port " << config.port_d();
 			}
-		}	
+		}
+		tmp_filter << ") ";
 	}
 
 	if(config.arp()){
@@ -57,36 +63,67 @@ Sniffer::Sniffer(conf::Config &config) : config{config}{
 		}
 		tmp_filter << "icmp ";
 	}
-	if(config.icmp6()){
-		if(tmp_filter.rdbuf()->in_avail() != 0){
-			tmp_filter << "or ";
-		}
-		tmp_filter << "icmp[0] = 8 or icmp[0] = 0 ";
-	}
 	if(config.igmp()){
 		if(tmp_filter.rdbuf()->in_avail() != 0){
 			tmp_filter << "or ";
 		}
 		tmp_filter << "igmp ";
 	}
-	if(config.ndp()){
-		//TODO
-		if(tmp_filter.rdbuf()->in_avail() != 0){
-			tmp_filter << "or ";
-		}
-		tmp_filter << "ndp ";
-	}
-	if(config.mld()){
-		if(tmp_filter.rdbuf()->in_avail() != 0){
-			tmp_filter << "or ";
-		}
-		tmp_filter << "mld ";
-	}
+
+    if (config.icmp6()) {
+        if (config.ndp() && config.mld()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "icmp6 ";
+        } else if (config.ndp() && !config.mld()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "((icmp6 and (icmp6[0] == 128 or icmp6[0] == 129)) or (icmp6 and (icmp6[0] >= 133 and icmp6[0] <= 137))) ";
+        } else if (!config.ndp() && config.mld()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "((icmp6 and (icmp6[0] == 128 or icmp6[0] == 129)) or (icmp6 and (icmp6[0] >= 130 and icmp6[0] <= 132))) ";
+        } else {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "(icmp6 and (icmp6[0] == 128 or icmp6[0] == 129)) ";
+        }
+    }
+	if (config.ndp() && config.mld()) {
+        if (!config.icmp6()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "(icmp6 and (icmp6[0] >= 130 and icmp6[0] <= 137)) ";
+        }
+    }
+
+    if (config.ndp()) {
+		if (!config.icmp6() && !config.mld()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "(icmp6 and (icmp6[0] >= 133 and icmp6[0] <= 137)) ";
+        }
+    }
+
+    if (config.mld()) {
+        if (!config.icmp6() && !config.ndp()) {
+			if(tmp_filter.rdbuf()->in_avail() != 0){
+				tmp_filter << "or ";
+			}
+            tmp_filter << "(icmp6 and (icmp6[0] >= 130 and icmp6[0] <= 132)) ";
+        }
+    }
 	#ifdef DEBUG
-	this->filter = tmp_filter.str();
+	std::cout << "BPF Filter: " << this->filter << std::endl;
 	#endif
 
-	std::cout << this->filter << std::endl;
+	this->filter = tmp_filter.str();
 }
 
 Sniffer::~Sniffer(){
